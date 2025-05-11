382dd06a5c5c841a3bc12992e7c44a809597dcbf
jonatasarruda <jonatasc.arruda@gmail.com>
2025-05-11 09:49:29 -0300

    Primeiros ajustes para frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <hello-world />
+    <v-btn color="primary" class="mt-5"> Botão Vuetify </v-btn>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
+
+<style scoped>
+.home {
+  text-align: center; /* Para centralizar o botão */
+}
+.mt-5 {
+  margin-top: 20px; /* Apenas para espaçamento */
+}
+</style>
