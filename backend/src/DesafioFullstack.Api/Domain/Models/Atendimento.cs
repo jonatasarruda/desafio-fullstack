49adf8b7e7f18e2bf267c571605b6affb6b62fc8
jonatasarruda <jonatasc.arruda@gmail.com>
2025-05-14 19:40:05 -0300

    Criacao do models restantes

--- backend/src/DesafioFullstack.Api/Domain/Models/Atendimento.cs
+++ backend/src/DesafioFullstack.Api/Domain/Models/Atendimento.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,20 @@ namespace DesafioFullstack.Api.Domain.Models
     public class Atendimento
     {
         [Key]
-        public long Id { get; set; }
-        [Required(ErrorMessage = "Campo de nome é obrigatório")]
-        public string Nome { get; set; } = string.Empty;
+        public long Id { get; set; }     
+        [Required(ErrorMessage = "Informe o texto de abertura do atendimento")]
+        public string TextoAberturaAtendimento { get; set;} = string.Empty;
         public DateTime DataCadastro { get; set; }
         public DateTime? DataInativacao { get; set; }
+        [Required(ErrorMessage = "Informe o usuário do atendimento")]
+        public long UsuarioId { get; set; }
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+        [Required(ErrorMessage = "Informe o cliente do atendimento")]
+        public long ClienteId { get; set; }
+        [ForeignKey("ClienteId")]
+        public Cliente Cliente { get; set; }
+        public List<Parecer> Pareceres { get; set; }
+        
     }
 }
---
+++ backend/src/DesafioFullstack.Api/Domain/Models/Parecer.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioFullstack.Api.Domain.Models
+{
+    public class Parecer
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição do parecer")]
+        public string DescricaoParecer { get; set;} = string.Empty;
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataInativacao { get; set; }
+
+        [Required(ErrorMessage = "Informe o usuário do parecer")]
+        public Usuario Usuario { get; set; }
+
+        [Required(ErrorMessage = "Informe o atendimento do parecer")]
+        public long AtendimentoId { get; set; }
+        [ForeignKey("AtendimentoId")]
+        public Atendimento Atendimento { get; set; }
+         
+    }
+}
